0352eb566f70a261702185e4b1a0050fb3265813
Mr_Starkk <yashshr09@gmail.com>
2021-06-16 22:16:17 +0530

    laayout changes

--- Etch-a-sketch.js
+++ Etch-a-sketch.js
@@ -1,18 +1,19 @@
-function squares(){
+function area(){
+    console.log("hello")
+    size = prompt('size?');
+    squares(size);
+}
+
+
+function squares(size = 16){
     const container = document.querySelector('#container');
-    for(i=0; i < 16*16; i++){
+    for(i=0; i < size*size; i++){
         const div = document.createElement('div');
         div.id = 'square';
+        div.addEventListener("mouseover", ()=> {div.classList.toggle('color');}, false )
         container.appendChild(div);
         
         
     }
 };
 squares();
-
-document.getElementById('square').addEventListener('hover', changebackground());
-
-
-function changebackground(){
-	document.getElementById('square').style.backgroundColor = 'green' ; 
-}
