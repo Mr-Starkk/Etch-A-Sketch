9c98bf2f2240a72bfeeff16c60f19fa8c4c904e9
Mr_Starkk <yashshr09@gmail.com>
2021-07-19 00:51:06 +0530

    Made again from ground-up.

--- Etch-a-sketch.js
+++ Etch-a-sketch.js
@@ -1,27 +1,47 @@
-function area(){
-    console.log("hello")
-    size = prompt('size?');
-    squares(size);
-}
+const mainContainer = document.getElementById('main-container');
+const eraseButton = document.getElementById('clear-button');
+const rangeSlider = document.getElementById('square-range-slider');
+const rangeSliderContainer = document.getElementById('range-slider-container');
+createGrid();
+const mainContainerSquares = document.querySelectorAll('#innersquares');
 
 
-function squares(size = 16){
-    const container = document.querySelector('#container');
-    for(i=0; i < size*size; i++){
-        const div = document.createElement('div');
-        div.id = 'square';
-        div.addEventListener("mouseover", changeColor )
-        container.appendChild(div);
-        
+
+
+
+function createGrid(squares = 32){
+    for(i=0; i<squares*squares; i++){
+        mainContainer.style.gridTemplateColumns = `repeat(${squares}, auto)`;
+        const newSquare = document.createElement('div');
+        newSquare.id = 'innersquares';
+        mainContainer.appendChild(newSquare);
         
     }
-};
-squares();
+}
+
+    mainContainerSquares.forEach((square) => {
+    square.addEventListener('mouseover', changeColor);
+    });
 
 function changeColor(e){
     let randomR = Math.floor(Math.random() * 256);
     let randomG = Math.floor(Math.random() * 256);
     let randomB = Math.floor(Math.random() * 256);
-    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG},${randomB})`;
+    e.target.style.background =  `radial-gradient(rgb(${randomR}, ${randomG},${randomB}), rgb(${randomR/2}, ${randomG/2},${randomB/2}))`;
 
-};
+}
+
+function clearGrid(){
+    mainContainer.innerHTML = "";
+}
+    eraseButton.addEventListener('click', () => {
+        clearGrid();
+        createGrid();
+    });
+
+rangeSlider.addEventListener('change', () => {
+        clearGrid();
+        createGrid(rangeSlider.valueAsNumber);
+        colorChangeOnHover();
+        rangeSliderContainer.textContent= ` Grid-Size : ${rangeSlider.valueAsNumber} `;
+    });
