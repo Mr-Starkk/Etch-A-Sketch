fa115cddf0533a76de54b05cb9720756fe4f2450
Mr_Starkk <yashshr09@gmail.com>
2021-07-20 02:09:52 +0530

    added font and background button support

--- Etch-a-sketch.js
+++ Etch-a-sketch.js
@@ -2,14 +2,16 @@ const mainContainer = document.getElementById('main-container');
 const eraseButton = document.getElementById('clear-button');
 const rangeSlider = document.getElementById('square-range-slider');
 const rangeSliderContainer = document.getElementById('range-slider-container');
-createGrid();
 let mainContainerSquares = document.querySelectorAll('#innersquares');
-addHoverEffect();
 const penColor = document.getElementById('pen-color');
+const backgroundColor = document.getElementById('background-color');
+const toggleBorder = document.getElementById('grid-border');
+createGrid();
+addHoverEffect();
+
 
 
         eraseButton.addEventListener('click', () => {
-            // removeHoverEffect();
             clearGrid();
             createGrid();
             addHoverEffect();
@@ -34,8 +36,18 @@ const penColor = document.getElementById('pen-color');
             
         })
         
+        backgroundColor.addEventListener('input', (e) => {
+            mainContainer.style.background = e.target.value;
+        })
+
+        toggleBorder.addEventListener('click', (e) => {
+            console.log(mainContainerSquares)
+            mainContainerSquares.forEach((square) => {
+                square.style.border = '1px solid transparent';
+            })
+        })
         
-        function createGrid(squares = 32){
+    function createGrid(squares = 32){
             for(i=0; i<squares*squares; i++){
                 mainContainer.style.gridTemplateColumns = `repeat(${squares}, auto)`;
                 const newSquare = document.createElement('div');
@@ -46,36 +58,36 @@ const penColor = document.getElementById('pen-color');
         }
         
 
-        function addHoverEffect(){
+    function addHoverEffect(){
             mainContainerSquares = document.querySelectorAll('#innersquares');
             mainContainerSquares.forEach((square) => {
                 square.addEventListener('mouseover', changeColor);
             });
         }
         
-        function removeHoverEffect(){
+    function removeHoverEffect(){
             mainContainerSquares = document.querySelectorAll('#innersquares');
             mainContainerSquares.forEach((square) => {
             square.removeEventListener('mouseover', changeColor);
-    });
-}
+        });
+    }
 
 
-function changeColor(e){
+    function changeColor(e){
     
-    let randomR = Math.floor(Math.random() * 256);
-    let randomG = Math.floor(Math.random() * 256);
-    let randomB = Math.floor(Math.random() * 256);
-    e.target.style.background =  `radial-gradient(rgb(${randomR}, ${randomG},${randomB}), rgb(${randomR/2}, ${randomG/2},${randomB/2}))`;
+        let randomR = Math.floor(Math.random() * 256);
+        let randomG = Math.floor(Math.random() * 256);
+        let randomB = Math.floor(Math.random() * 256);
+        e.target.style.background =  `radial-gradient(rgb(${randomR}, ${randomG},${randomB}), rgb(${randomR/2}, ${randomG/2},${randomB/2}))`;
     
-}
+    }
 
 
-function clearGrid(){
-    mainContainer.innerHTML = "";
-}
+    function clearGrid(){
+        mainContainer.innerHTML = "";
+    }
 
 
-function addPenColor(e, selectedColor){
-    e.target.style.background =  `${selectedColor}`;
-}
+    function addPenColor(e, selectedColor){
+        e.target.style.background =  `${selectedColor}`;
+    }
